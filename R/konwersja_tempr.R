cd709ac9d1f949e98a039e66d4bfde368f8427bd
jakub talaga <ja.talaga777@gmail.com>
2024-02-08 09:49:41 +0100

    Initial commit

--- R/konwersja_tempr.R
+++ R/konwersja_tempr.R
@@ -13,5 +13,22 @@ konwersja_temp = function(temperatura_f){
   print(ggplot2::ggplot(x, ggplot2::aes(t)) + ggplot2::geom_histogram())
 }
 konwersja_temp(50)
+
+
+#połącznie projektu z github
 usethis::use_git()
 usethis::use_github()
+
+#Plik Readme
+usethis::use_readme_rmd()
+
+usethis::use_news_md()
+usethis::use_vignette("winieta_testowa")
+
+moj_df = data.frame(x=1:50,y=51:100)
+moj_df2 = data.frame(z=1:5,q=LETTERS [1:5])
+moj_df2
+usethis::use_data(moj_df,overwrite = TRUE)
+system.file(package = "spData")
+write.csv(moj_df,file="C:/Users/Student/Desktop/Git_pakietyR/pietruszka/inst/data.csv")
+usethis::use_data(moj_df, moj_df2, internal = TRUE)
